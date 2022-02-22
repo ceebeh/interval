c7e7c3f0f70402ee9aa07a4c9e1372d7451b19bf
schro <schro@192.168.0.125>
2022-02-22 17:27:07 +0100

    First commit

---
+++ interval/src/interval/intervalTest.java
@@ -0,0 +1,19 @@
+package interval;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class intervalTest {
+	// Verschillende voorstellingen
+	class Interval {
+		int ondergrens;
+		int bovengrens;
+	}
+	
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
